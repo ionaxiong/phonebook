569ce3873252e362214a42de39e5b55a2ef28eb7
Ming Xiong <iona.m.xiong@gmail.com>
2021-07-31 21:59:12 +0300

    3.15

--- index.js
+++ index.js
@@ -116,6 +116,14 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port${PORT}`);
